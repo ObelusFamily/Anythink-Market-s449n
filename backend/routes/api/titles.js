0d4e31c75f32b49f88a2fcbb19d4563e7398f971
Rob Wallator <63985483+88Navistar@users.noreply.github.com>
2022-09-03 20:14:34 -0600

    change

---
+++ backend/routes/api/titles.js
@@ -0,0 +1,12 @@
+var router = require('express').Router();
+var mongoose = require('mongoose');
+var Item = mongoose.model('Item');
+
+// return a list of titles
+router.get('/', function(req, res, next) {
+  Item.find().distinct('titleList').then(function(titles){
+    return res.json({titles: titles});
+  }).catch(next);
+});
+
+module.exports = router;
